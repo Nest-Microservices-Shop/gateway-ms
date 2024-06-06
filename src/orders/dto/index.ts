6d69ba10b5785d2e4a619bd594d583b1fb67eac7
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-06-06 08:54:10 -0400

    feat: index for dtos

---
+++ src/orders/dto/index.ts
@@ -0,0 +1,6 @@
+export { OrderPaginationDto } from './order-pagination.dto';
+
+export { StatusDto } from './status.dto';
+
+export { CreateOrderDto } from './create-order.dto';
+export { UpdateOrderDto } from './update-order.dto';

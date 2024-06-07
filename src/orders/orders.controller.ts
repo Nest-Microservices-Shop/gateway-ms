214bc004c2f565c7690725375b0ea2a2eec33390
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-06-07 10:37:26 -0400

    feat: changed order service by nats service

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -9,7 +9,7 @@ import {
   Query,
   Patch,
 } from '@nestjs/common';
-import { ORDER_SERVICE } from 'src/config';
+import { NATS_SERVICE } from 'src/config';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { CreateOrderDto, OrderPaginationDto, StatusDto } from './dto';
 import { PaginationDto } from 'src/common';
@@ -17,26 +17,21 @@ import { catchError } from 'rxjs';
 
 @Controller('orders')
 export class OrdersController {
-  constructor(
-    @Inject(ORDER_SERVICE) private readonly ordersClient: ClientProxy,
-  ) {}
+  constructor(@Inject(NATS_SERVICE) private readonly Client: ClientProxy) {}
 
   @Post()
   create(@Body() createOrderDto: CreateOrderDto) {
-    return this.ordersClient.send({ cmd: 'create_order' }, createOrderDto);
+    return this.Client.send({ cmd: 'create_order' }, createOrderDto);
   }
 
   @Get()
   findAll(@Query() orderPaginationDto: OrderPaginationDto) {
-    return this.ordersClient.send(
-      { cmd: 'find_all_orders' },
-      orderPaginationDto,
-    );
+    return this.Client.send({ cmd: 'find_all_orders' }, orderPaginationDto);
   }
 
   @Get('id/:id')
   findOne(@Param('id', ParseUUIDPipe) id: string) {
-    return this.ordersClient.send({ cmd: 'find_order' }, { id }).pipe(
+    return this.Client.send({ cmd: 'find_order' }, { id }).pipe(
       catchError((err) => {
         throw new RpcException(err);
       }),
@@ -49,7 +44,7 @@ export class OrdersController {
     @Query() paginationDto: PaginationDto,
   ) {
     try {
-      return this.ordersClient.send(
+      return this.Client.send(
         { cmd: 'find_all_orders' },
         {
           ...paginationDto,
@@ -67,7 +62,7 @@ export class OrdersController {
     @Body() statusDto: StatusDto,
   ) {
     try {
-      return this.ordersClient.send(
+      return this.Client.send(
         { cmd: 'change_order_status' },
         {
           id,
